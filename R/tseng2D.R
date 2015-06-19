2bb40c2498b2bb56e46105f5a50111e95496c402
PhilipPallmann <p.pallmann@lancaster.ac.uk>
2015-06-19 18:09:14 +0100

    Tseng

---
+++ R/tseng2D.R
@@ -0,0 +1,41 @@
+tseng2D <- function(dat, alpha=0.1, steps=400, equi=1.25, plotrange=c(0.77, 1.3), axnames=NULL){
+  
+  if(ncol(dat)!=2){
+    stop("Data must be bivariate.")
+  }
+  
+  n <- nrow(dat)
+  df <- n - 1
+  
+  est <- matrix(colMeans(dat), 2)
+  poolvar <- var(as.vector(as.matrix(dat)))
+  s2 <- poolvar / n
+  
+  togrid <- list()
+  
+  for(i in 1:2){
+    togrid[[i]] <- seq(est[i] - 8 * poolvar, est[i] + 8 * poolvar, length.out=steps)
+  }
+  
+  grid <- expand.grid(togrid)
+  
+  findcrTse <- apply(grid, 1, function(x){
+    theta <- x
+    (sqrt(sum(est^2))^2 / (2 * s2)) > qf(p=alpha, df1=2, df2=df, ncp=(sqrt(sum(theta^2))^2 / s2))
+  })
+  
+  crTse <- cbind(grid, findcrTse)[findcrTse==1, ]
+  
+  if(is.null(axnames)==TRUE){
+    axisnames <- colnames(dat)
+  }else{
+    axisnames <- axnames
+  }
+    
+  plot(0, xlim=log(plotrange), ylim=log(plotrange), las=1, xlab=axisnames[1], ylab=axisnames[2],
+       cex.main=2.5, cex.axis=1.5, cex.lab=1.5, main="Tseng")
+  rect(log(1/equi), log(1/equi), log(equi), log(equi), col="gray95", border="gray95")
+  points(crTse[, -3])
+  points(est, pch=16, col="white")
+  
+}
