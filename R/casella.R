76ceddf4d97819ed206622a6c5f3fd7cce84e593
PhilipPallmann <p.pallmann@lancaster.ac.uk>
2015-06-22 12:10:57 +0100

    Casella

---
+++ R/casella.R
@@ -0,0 +1,88 @@
+casella <- function(dat, alpha=0.1, steps=100){
+  
+  n <- nrow(dat)
+  p <- ncol(dat)
+  df <- n - 1
+  
+  est <- matrix(colMeans(dat), p)
+  poolvar <- var(as.vector(as.matrix(dat)))
+  s <- sqrt(poolvar / n)
+  
+  togrid <- list()
+  
+  for(i in 1:p){
+    togrid[[i]] <- seq(est[i] - 2 * poolvar, est[i] + 2 * poolvar, length.out=steps)
+  }
+  
+  grid <- expand.grid(togrid)
+  
+  findcrCas <- apply(grid, 1, function(x){
+    theta <- matrix(x, 2)
+    sqrt(sum((theta - est)^2)) < (s * sqrt(2 * qf(p=1 - alpha, df1=2, df2=df)))
+  })
+  
+  crCas <- cbind(grid, findcrCas)[findcrCas==1, ]
+  
+  Cas0 <- t(apply(crCas[, 1:2], 2, range, na.rm=TRUE))
+  
+  if(min(abs(Cas0[, 1] - est + 2 * poolvar)) < 0.001 | min(abs(Cas0[, 2] - est - 2 * poolvar)) < 0.001){
+    
+    togrid2 <- list()
+    
+    for(i in 1:p){
+      togrid2[[i]] <- seq(est[i] - 8 * poolvar, est[i] + 8 * poolvar, length.out=4 * steps)
+    }
+    
+    grid2 <- expand.grid(togrid2)
+    
+    findcrCas2 <- apply(grid2, 1, function(x){
+      theta <- matrix(x, 2)
+      sqrt(sum((theta - est)^2)) < (s * sqrt(2 * qf(p=1 - alpha, df1=2, df2=df)))
+    })
+    
+    crCas2 <- cbind(grid2, findcrCas2)[findcrCas2==1, ]
+    
+    Cas0 <- t(apply(crCas2[, -(p + 1)], 2, range))
+    
+  }
+  
+  stepwidth <- 4 * poolvar / steps
+  
+  if(p==2){
+    
+    GridA <- expand.grid(seq(Cas0[1, 1] - stepwidth, Cas0[1, 1], length.out=steps),
+                         seq(Cas0[2, 1] - stepwidth, Cas0[2, 2], length.out=steps))
+    
+    GridB <- expand.grid(seq(Cas0[1, 2], Cas0[1, 2] + stepwidth, length.out=steps),
+                         seq(Cas0[2, 1], Cas0[2, 2] + stepwidth, length.out=steps))
+    
+    GridC <- expand.grid(seq(Cas0[1, 1] - stepwidth, Cas0[1, 2], length.out=steps),
+                         seq(Cas0[2, 1] - stepwidth, Cas0[2, 1], length.out=steps))
+    
+    GridD <- expand.grid(seq(Cas0[1, 1], Cas0[1, 2] + stepwidth, length.out=steps),
+                         seq(Cas0[2, 2], Cas0[2, 2] + stepwidth, length.out=steps))
+    
+    Grid <- rbind(GridA, GridB, GridC, GridD)
+    
+    FindcrCas <- apply(Grid, 1, function(x){
+      theta <- matrix(x, 2)
+      sqrt(sum((theta - est)^2)) < (s * sqrt(2 * qf(p=1 - alpha, df1=2, df2=df)))
+    })
+    
+    CrCas <- cbind(Grid, FindcrCas)[FindcrCas==1, ]
+    
+    Cas <- t(apply(CrCas[, -(p + 1)], 2, range))
+    
+  }else{
+    
+    Cas <- Cas0
+    
+  }
+  
+  CasOut <- cbind(est, Cas)
+  rownames(CasOut) <- colnames(dat)
+  colnames(CasOut) <- c("estimate", "lower", "upper")
+  
+  return(CasOut)
+  
+}
---
+++ R/casella2D.R
@@ -0,0 +1,41 @@
+casella2D <- function(dat, alpha=0.1, steps=400, equi=1.25, plotrange=c(0.77, 1.3), axnames=NULL){
+  
+  if(ncol(dat)!=2){
+    stop("Data must be bivariate.")
+  }
+  
+  n <- nrow(dat)
+  df <- n - 1
+  
+  est <- matrix(colMeans(dat), 2)
+  poolvar <- var(as.vector(as.matrix(dat)))
+  s <- sqrt(poolvar / n)
+  
+  togrid <- list()
+  
+  for(i in 1:2){
+    togrid[[i]] <- seq(est[i] - 8 * poolvar, est[i] + 8 * poolvar, length.out=steps)
+  }
+  
+  grid <- expand.grid(togrid)
+  
+  findcrCas <- apply(grid, 1, function(x){
+    theta <- matrix(x, 2)
+    sqrt(sum((theta - est)^2)) < (s * sqrt(2 * qf(p=1 - alpha, df1=2, df2=df)))
+  })
+  
+  crCas <- cbind(grid, findcrCas)[findcrCas==1, ]
+  
+  if(is.null(axnames)==TRUE){
+    axisnames <- colnames(dat)
+  }else{
+    axisnames <- axnames
+  }
+  
+  plot(0, xlim=log(plotrange), ylim=log(plotrange), las=1, xlab=axisnames[1], ylab=axisnames[2],
+       cex.main=2.5, cex.axis=1.5, cex.lab=1.5, main="Casella")
+  rect(log(1/equi), log(1/equi), log(equi), log(equi), col="gray95", border="gray95")
+  points(crCas[, -3])
+  points(est, pch=16, col="white")
+  
+}
