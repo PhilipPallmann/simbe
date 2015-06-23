0138b80cf4171ce4eecc63e3dff1b60e1c2a6731
PhilipPallmann <p.pallmann@lancaster.ac.uk>
2015-06-23 11:01:48 +0100

    Gray is the new black

--- R/casella2D.R
+++ R/casella2D.R
@@ -34,7 +34,7 @@ casella2D <- function(dat, alpha=0.1, steps=400, equi=1.25, plotrange=c(0.77, 1.
   
   plot(0, xlim=log(plotrange), ylim=log(plotrange), las=1, xlab=axisnames[1], ylab=axisnames[2],
        cex.main=2.5, cex.axis=1.5, cex.lab=1.5, main="Casella")
-  points(crCas[, -3], pch=20)
+  points(crCas[, -3], pch=20, col="gray50")
   rect(log(1/equi), log(1/equi), log(equi), log(equi), col="gray95", border=NA, density=50)
   points(est, pch=19, col="white")
   
